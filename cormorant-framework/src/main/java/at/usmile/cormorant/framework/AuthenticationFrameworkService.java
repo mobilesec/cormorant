4d03dc0bb4cdeb2b46e8dc9661ea4723f71f4a77
Daniel Hintze <daniel@hintze-it.de>
2017-03-06 12:05:45 +0100

    Add / reformat licence header

--- cormorant-framework/src/main/java/at/usmile/cormorant/framework/AuthenticationFrameworkService.java
+++ cormorant-framework/src/main/java/at/usmile/cormorant/framework/AuthenticationFrameworkService.java
@@ -1,15 +1,15 @@
 /**
  * Copyright 2016 - Daniel Hintze <daniel.hintze@fhdw.de>
- * Sebastian Scholz <sebastian.scholz@fhdw.de>
- * Rainhard D. Findling <rainhard.findling@fh-hagenberg.at>
- * Muhammad Muaaz <muhammad.muaaz@usmile.at>
- * <p>
+ * 				 Sebastian Scholz <sebastian.scholz@fhdw.de>
+ * 				 Rainhard D. Findling <rainhard.findling@fh-hagenberg.at>
+ * 				 Muhammad Muaaz <muhammad.muaaz@usmile.at>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
